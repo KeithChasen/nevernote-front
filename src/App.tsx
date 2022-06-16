8b57884578d501b684d64a30a14a26ad5d17948f
keithchasen <keithchasen89@gmail.com>
2022-06-16 17:37:54 +0300

    basic router setup for login sign up and home pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='login' element={<Login />}/>
+        <Route path='signup' element={<Signup />}/>
+        <Route index element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Signup() {
+    return (
+        <div>
+            <h1>SIGNUP PAGE</h1>
+        </div>
+    );
+}

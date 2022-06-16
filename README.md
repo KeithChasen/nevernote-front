e7d911ac2d69ef9f85868f8305a51861e3f24f63
keithchasen <keithchasen89@gmail.com>
2022-06-16 23:56:17 +0300

    login

--- README.md
+++ README.md
@@ -44,3 +44,12 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+
+## GraphQL Code gen
+
+`yarn add -D @graphql-codegen/cli`
+
+### run:
+
+`npx graphql-codegen init`
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+import { useLoginMutation } from "../generated/graphql";
 
 export function Login() {
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+    const [submitLogin, { error }] = useLoginMutation()
+
+    const onSubmitHandler = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            await submitLogin({
+                variables: {
+                    ...form
+                }
+            })
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    const onChangeHandler = (name: string) =>
+        ({ target }: ChangeEvent<HTMLInputElement>) =>
+            setForm({ ...form, [name]: target.value })
+
     return (
         <div>
-            <h1>LOGIN PAGE</h1>
+            <form onSubmit={onSubmitHandler}>
+                <div>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={form.email}
+                        onChange={onChangeHandler('email')}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={form.password}
+                        onChange={onChangeHandler('password')}
+                    />
+                </div>
+
+                { error && <div>{JSON.stringify(error.message)}</div> }
+
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
         </div>
     );
 }
